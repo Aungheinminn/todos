3a029926e2160f4ee3f06d5a736041eb56062c93
Aungheinminn <aungheinnmin@gmail.com>
2024-07-26 23:24:16 +0630

    styling day picker

---
+++ src/local_consts/daypicker.styles.ts
@@ -0,0 +1,30 @@
+export const dayStyle = {
+    fontWeight: "normal",
+    margin: "10px",
+    color: "#34aeeb"
+};
+export const headStyle = {
+    display: "flex",
+    justifyContent: "space-between",
+    fontSize: "1.2rem",
+    fontWeight: "bold",    
+    margin: "0 0 10px 0",
+    color: "#34aeeb",
+}
+export const navStyle = {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItem: "center",
+    gap: "0 10px",
+    color: "#34aeeb" 
+}
+export const headCellStyle = {
+    color: "#34aeeb"
+}
+export const monthStyle = {
+    width: "auto",
+    background: "#2C3E50",
+    border: "2px solid #34aeeb",
+    borderRadius: "10px",
+    padding: "10px 12px"
+}
