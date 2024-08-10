15ed497e54db53614ae9094b6f6255e0918ba3d3
Aungheinminn <aungheinnmin@gmail.com>
2024-08-10 23:17:28 +0630

    implement generic popup

--- src/components/Popup/Popup.tsx
+++ src/components/Popup/Popup.tsx
@@ -12,16 +12,63 @@ import {
 import { Button } from "@/components/ui/button"
 import { RoutineType } from "@/lib/types/routine.type";
 import { useCurrentUserStore } from "@/lib/userStore";
-import { useState } from "react";
+import React, { SetStateAction, useState } from "react";
+import { Dropdown } from "react-day-picker";
+import DropdownComponent from "../DropdownComponent/DropdownComponent";
 
 type PopupComponentProps = {
+    type: 'plan' | 'routine' | 'activity';
     process: (data: RoutineType) => void;
     trigger: string;
+    items?: any;
 }
 
-const PopupComponent:React.FC<PopupComponentProps> = ({ process, trigger}) => {
+type PlanPopupProps = {
+    name: string;
+    setName: React.Dispatch<SetStateAction<string>>;
+}
+
+type RoutinePopupProps = {
+    name: string;
+    setName: React.Dispatch<SetStateAction<string>>;
+    items: any;
+    onSelectRoutine: (value: string) => void;
+}
+
+const PlanPopup: React.FC<PlanPopupProps> = ({ name, setName}) => {
+    return(
+        <AlertDialogHeader>
+            <AlertDialogTitle>Create a plan</AlertDialogTitle>
+            <div className="flex flex-col">
+                <label className="mb-2" htmlFor="name">Name</label>
+                <input className="text-black p-1 py-2 rounded-md" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+            </div>
+        </AlertDialogHeader>
+    )
+}
+
+const RoutinePopup: React.FC<RoutinePopupProps> = ({ name, setName, items, onSelectRoutine}) => {
+    return(
+        <AlertDialogHeader>
+            <AlertDialogTitle>Create a routine</AlertDialogTitle>
+            <div className="flex flex-col">
+                <label className="mb-2" htmlFor="name">Name</label>
+                <input className="text-black py-2 px-3 rounded-md w-full focus:outline-[#0ea5e9]" type="text" value={name} onChange={(e) => setName(e.target.value)} />
+            </div>
+            <DropdownComponent title="Select a Routine" items={items} onSelectItem={onSelectRoutine} />
+        </AlertDialogHeader>
+    )
+}
+
+const PopupComponent:React.FC<PopupComponentProps> = ({ type, process, trigger, items }) => {
     const { currentUser } = useCurrentUserStore(state=> state) 
     const [name, setName] = useState<string>('')
+    const [selectedRoutine, setSelectedRoutine] = useState<string>('')
+
+    const handleSelectRoutine = (value: string) => {
+        setSelectedRoutine(value)
+    }
+
     const handleProcess = () => {
         const data = {
             name: name,
@@ -36,17 +83,12 @@ const PopupComponent:React.FC<PopupComponentProps> = ({ process, trigger}) => {
         <AlertDialogTrigger asChild>
             <Button className="w-[90px] h-[40px] bg-[#0ea5e9] border-0 rounded-lg p-2">{trigger}</Button>
         </AlertDialogTrigger>
-        <AlertDialogContent className="bg-[#334155] rounded-lg">
-            <AlertDialogHeader>
-                <AlertDialogTitle>Create a routine</AlertDialogTitle>
-                <div className="flex flex-col">
-                    <label className="mb-2" htmlFor="name">Name</label>
-                    <input className="text-black p-1 py-2 rounded-md" type="text" value={name} onChange={(e) => setName(e.target.value)} />
-                </div>
-            </AlertDialogHeader>
-            <AlertDialogFooter>
-                <AlertDialogCancel className="text-black" onClick={() => setName('')}>Cancel</AlertDialogCancel>
-                <AlertDialogAction onClick={handleProcess}>Continue</AlertDialogAction>
+        <AlertDialogContent className="w-[320px] bg-[#334155] rounded-lg">
+            {type === 'plan' && <PlanPopup name={name} setName={setName} />}
+            {type === 'routine' && <RoutinePopup name={name} setName={setName} items={items} onSelectRoutine={handleSelectRoutine} />}
+            <AlertDialogFooter className="w-full flex flex-row justify-ceter items-center gap-x-1">
+                <AlertDialogCancel className="text-black w-1/2 m-0 p-0" onClick={() => setName('')}>Cancel</AlertDialogCancel>
+                <AlertDialogAction className="w-1/2 m-0 p-0" onClick={handleProcess}>Continue</AlertDialogAction>
             </AlertDialogFooter>
         </AlertDialogContent>
         </AlertDialog>
