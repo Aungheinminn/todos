acc956bc5c7e2546a75dbeb9f09d91f02c5fabb5
Aungheinminn <aungheinnmin@gmail.com>
2024-09-17 22:36:30 +0630

    delete plan api and universal not found component

--- src/app/plans/[slug]/page.tsx
+++ src/app/plans/[slug]/page.tsx
@@ -20,6 +20,7 @@ import Loading from "@/components/Loading/Loading"
 import PlanLoading from "./loading"
 import { useActionPopupStore, usePopupStore } from "@/lib/popupStore"
 import { Button } from "@/components/ui/button"
+import NotFound from "@/components/NotFound/NotFound"
 
 type PlanHeaderProps = {
     plan: any,
@@ -124,46 +125,49 @@ const PlanRoutines:React.FC<PlanRoutinesProps> = ({ routines, plan, onCreate }) 
                 <p className="text-lg font-medium text-[#0ea5e9]">{plan.name}'s daily routines</p>
                 <Button className="bg-[#0ea5e9]" onClick={handleActionPopup}>Add</Button>
             </div>
-            <div className="cursor-pointer w-full flex flex-col gap-y-1">
-                {
-                    routines && routines.map((routine) => (
-                        <div key={routine._id} onClick={() => handleOpenDetailPopup(routine)} className="relative transition duration-300 ease-in-out group hover:border-[#C0C0C0] w-full h-[45px] flex items-center justify-start border-2 border-b-4 border-[#0ea5e9] rounded-md">
-                            <p className="w-[120px] truncate transition duration-300 ease-in-out text-md font-medium text-[#0ea5e9] m-2 group-hover:opacity-0">{routine.name}</p>
-                            <p className="absolute left-1/2 -translate-x-1/2 transition duration-300 ease-in-out text-md font-medium text-[#C0C0C0] m-2 opacity-0 group-hover:opacity-100"> Click for details</p>
-                        </div>
-                    ))
-                }
-            </div>
+            { routines && routines.length === 0 ? 
+                <NotFound context="No Routine is found!" /> :
+                <div className="cursor-pointer w-full flex flex-col gap-y-1">
+                    {
+                        routines && routines.map((routine) => (
+                            <div key={routine._id} onClick={() => handleOpenDetailPopup(routine)} className="relative transition duration-300 ease-in-out group hover:border-[#C0C0C0] w-full h-[45px] flex items-center justify-start border-2 border-b-4 border-[#0ea5e9] rounded-md">
+                                <p className="w-[120px] truncate transition duration-300 ease-in-out text-md font-medium text-[#0ea5e9] m-2 group-hover:opacity-0">{routine.name}</p>
+                                <p className="absolute left-1/2 -translate-x-1/2 transition duration-300 ease-in-out text-md font-medium text-[#C0C0C0] m-2 opacity-0 group-hover:opacity-100"> Click for details</p>
+                            </div>
+                        ))
+                    }
+                </div> 
+            }
 
         </div>
     )
 }
 
-const PlanDailyActivityItems:React.FC<PlanDailyActivityItemsProps> = ({ plan }) => {
-    const colors = ['#f87171', '#bef264', '#86efac', '#0ea5e9', '#22d3ee', '#8b5cf6', '#f43f5e', '#f472b6', '#701a75', '#083344']
+// const PlanDailyActivityItems:React.FC<PlanDailyActivityItemsProps> = ({ plan }) => {
+//     const colors = ['#f87171', '#bef264', '#86efac', '#0ea5e9', '#22d3ee', '#8b5cf6', '#f43f5e', '#f472b6', '#701a75', '#083344']
 
-    const randomColor = () => {
-        return colors[Math.floor(Math.random() * colors.length)]
-    }
-    return (
-        <div className="w-full flex flex-col p-1 gap-y-2 p-1">
-            <p className="text-lg font-medium text-[#0ea5e9]">{plan.name}'s daily activity items</p>
-            <div className="flex flex-wrap gap-1">
-                { Array.from({ length: 10}).map((_, index) => (
-                    <Badge key={index} style={{
-                        backgroundColor: randomColor()
-                    }} className="cursor-pointer h-[30px] line-clamp-1 flex justify-self-stretch items-center justify-center text-md rounded-3xl">
-                        <TooltipComponent text={`text - ${ index }`} />
-                    </Badge>
-                ))}
-                <Badge className="cursor-pointer m-[2px] h-[30px] flex justify-between items-center gap-x-1 text-sm rounded-3xl bg-[#0eade9]">
-                    <Image src={add} alt="add" className="w-5 h-5" />
-                    <p className="mr-1">Add</p>
-                </Badge>
-            </div>
-        </div>
-    )
-}
+//     const randomColor = () => {
+//         return colors[Math.floor(Math.random() * colors.length)]
+//     }
+//     return (
+//         <div className="w-full flex flex-col p-1 gap-y-2 p-1">
+//             <p className="text-lg font-medium text-[#0ea5e9]">{plan.name}'s daily activity items</p>
+//             <div className="flex flex-wrap gap-1">
+//                 { Array.from({ length: 10}).map((_, index) => (
+//                     <Badge key={index} style={{
+//                         backgroundColor: randomColor()
+//                     }} className="cursor-pointer h-[30px] line-clamp-1 flex justify-self-stretch items-center justify-center text-md rounded-3xl">
+//                         <TooltipComponent text={`text - ${ index }`} />
+//                     </Badge>
+//                 ))}
+//                 <Badge className="cursor-pointer m-[2px] h-[30px] flex justify-between items-center gap-x-1 text-sm rounded-3xl bg-[#0eade9]">
+//                     <Image src={add} alt="add" className="w-5 h-5" />
+//                     <p className="mr-1">Add</p>
+//                 </Badge>
+//             </div>
+//         </div>
+//     )
+// }
 
 const Plan = ({ params }: {
     params: { slug: string}
@@ -237,7 +241,7 @@ const Plan = ({ params }: {
                 <PlanHeader plan={plan} isPlanLoading={isPlanLoading} />
                 <PlanOverview plan={plan} />
                 <PlanRoutines routines={routines} plan={plan} onCreate={handleCreateRoutine} />
-                <PlanDailyActivityItems plan={plan} />
+                {/* <PlanDailyActivityItems plan={plan} /> */}
             </div>
         </Suspense>
     )
