7c8df1cef14c416a3d004685981aaeb24907dced
Aungheinminn <aungheinnmin@gmail.com>
2024-09-24 19:36:35 +0630

    finalized edit plan api implementation

--- src/lib/models/activePlan.model.ts
+++ src/lib/models/activePlan.model.ts
@@ -1,10 +1,12 @@
 import { z } from "zod";
 
 export const PlanSchema = z.object({
+    _id: z.string().optional(),
     name: z.string().min(1),
     description: z.string().nullable().optional(),
     icon: z.string().nullable().optional(),
     user_id: z.string().min(1),
+    createdAt: z.string().optional()
 })
 
 export type PlanModel = z.infer<typeof PlanSchema>;
