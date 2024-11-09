7e6a16f6386adc3625aec0c9ed7b18f0be342b6c
Aungheinminn <aungheinnmin@gmail.com>
2024-11-09 22:11:03 +0630

    update plan schema

--- src/lib/models/activePlan.model.ts
+++ src/lib/models/activePlan.model.ts
@@ -1,12 +1,23 @@
 import { z } from "zod";
 
+export const CreatePlanSchema = z.object({
+  name: z.string().min(1),
+  description: z.string().nullable().optional(),
+  icon: z.string().nullable().optional(),
+  user_id: z.string().min(1),
+  createdAt: z.string().optional(),
+});
+
+export type CreatePlanModel = z.infer<typeof CreatePlanSchema>;
+
 export const PlanSchema = z.object({
-    _id: z.string().optional(),
-    name: z.string().min(1),
-    description: z.string().nullable().optional(),
-    icon: z.string().nullable().optional(),
-    user_id: z.string().min(1),
-    createdAt: z.string().optional()
-})
+  _id: z.string().optional(),
+  name: z.string().min(1),
+  description: z.string().nullable().optional(),
+  icon: z.string().nullable().optional(),
+  user_id: z.string().min(1),
+  createdAt: z.string().optional(),
+});
 
 export type PlanModel = z.infer<typeof PlanSchema>;
+
