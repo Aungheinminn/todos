17444b0067886947c06e7d2269653a0efd074eff
Aungheinminn <aungheinnmin@gmail.com>
2024-11-17 14:07:54 +0630

    items selection ui for drawer

--- src/app/api/protected/routines/plan/[plan_id]/route.ts
+++ src/app/api/protected/routines/plan/[plan_id]/route.ts
@@ -3,87 +3,147 @@ import { RoutineSchema } from "@/lib/models/routine.model";
 import { ObjectId } from "mongodb";
 import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async (req: NextRequest, { params }: {
+export const GET = async (
+  req: NextRequest,
+  {
+    params,
+  }: {
     params: { plan_id: string };
-}) => {
-    
-    if(!params) {
-        return NextResponse.json({ error: "Plan ID is required" }, { status: 400 });
+  },
+) => {
+  if (!params) {
+    return NextResponse.json({ error: "Plan ID is required" }, { status: 400 });
+  }
+  const { searchParams } = new URL(req.url);
+  const searchKey = searchParams.get("search");
+  const { plan_id } = params;
+
+  try {
+    const client = await clientPromise;
+    const db = client.db("remarker_next");
+    const plans = await db
+      .collection("routines")
+      .find({
+        plan_id: plan_id,
+        ...(searchKey && { name: { $regex: searchKey, $options: "i" } }),
+      })
+      .sort({ _id: -1 })
+      .toArray();
+
+    if (!plans) {
+      return NextResponse.json(
+        { message: "No routines are found" },
+        { status: 404 },
+      );
     }
-    const { plan_id } = params    
-    
-    try {
-        const client = await clientPromise;
-        const db = client.db('remarker_next');
-        const plans = await db.collection('routines').find({ plan_id: plan_id }).toArray();
-        
-        if(!plans) {
-            return NextResponse.json({ message: "No routines are found" }, { status: 404 });
-        }
 
-        return NextResponse.json({ message: "Routines are successfully fetched", data: plans }, { status: 200 });
-    } catch (e) {
-        console.error(e);
-        return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
-    }   
-}
+    return NextResponse.json(
+      { message: "Routines are successfully fetched", data: plans },
+      { status: 200 },
+    );
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+};
 
-
-export const PUT = async (req: NextRequest, { params }: {
+export const PUT = async (
+  req: NextRequest,
+  {
+    params,
+  }: {
     params: {
-        id: string;
+      id: string;
+    };
+  },
+) => {
+  if (!params) {
+    return NextResponse.json(
+      { error: "Routine ID is required" },
+      { status: 400 },
+    );
+  }
+
+  const { id } = params;
+  const routineData = RoutineSchema.parse(req.body);
+
+  try {
+    const client = await clientPromise;
+    const db = client.db("remarker_next");
+    const routine = await db
+      .collection("routines")
+      .findOneAndUpdate(
+        { _id: new ObjectId(id) },
+        { $set: routineData },
+        { returnDocument: "after" },
+      );
+
+    if (!routine) {
+      return NextResponse.json(
+        { message: "Routine not found" },
+        { status: 404 },
+      );
     }
-}) => {
-    if(!params) {
-        return NextResponse.json({ error: "Routine ID is required" }, { status: 400 });
-    }
 
-    const { id } = params;
-    const routineData = RoutineSchema.parse(req.body);
+    return NextResponse.json(
+      { message: "Routine successfully updated", data: routine },
+      { status: 200 },
+    );
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+};
 
-    try {
-        const client = await clientPromise;
-        const db = client.db('remarker_next');
-        const routine = await db.collection('routines').findOneAndUpdate(
-            { _id: new ObjectId(id)},
-            { $set: routineData },
-            { returnDocument: 'after' }
-        );
-
-        if(!routine) {
-            return NextResponse.json({ message: "Routine not found" }, { status: 404 });
-        }
-
-        return NextResponse.json({ message: "Routine successfully updated", data: routine }, { status: 200 });
-    } catch (e) {
-        console.error(e);
-        return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
-    }
-}
-
-export const DELETE = async (req: NextRequest, { params }: {
+export const DELETE = async (
+  req: NextRequest,
+  {
+    params,
+  }: {
     params: {
-        id: string;
+      id: string;
+    };
+  },
+) => {
+  if (!params) {
+    return NextResponse.json(
+      { error: "Routine ID is required" },
+      { status: 400 },
+    );
+  }
+
+  const { id } = params;
+
+  try {
+    const client = await clientPromise;
+    const db = client.db("remarker_next");
+    const routine = await db
+      .collection("routines")
+      .findOneAndDelete({ _id: new ObjectId(id) });
+
+    if (!routine) {
+      return NextResponse.json(
+        { message: "Routine not found" },
+        { status: 404 },
+      );
     }
-}) => {
-    if(!params) {
-        return NextResponse.json({ error: "Routine ID is required" }, { status: 400 });
-    }
 
-    const { id } = params;
+    return NextResponse.json(
+      { message: "Routine successfully deleted", data: routine },
+      { status: 200 },
+    );
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+};
 
-    try {
-        const client = await clientPromise;
-        const db = client.db('remarker_next');
-        const routine = await db.collection('routines').findOneAndDelete({ _id: new ObjectId(id) });
-
-        if(!routine) {
-            return NextResponse.json({ message: "Routine not found" }, { status: 404 });
-        }
-
-        return NextResponse.json({ message: "Routine successfully deleted", data: routine }, { status: 200 });
-    } catch (e) {
-        console.error(e);
-        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
-    }
-}
--- src/lib/routines.service.ts
+++ src/lib/routines.service.ts
@@ -1,66 +1,85 @@
 import { RoutineType } from "./types/routine.type";
 
 export const createRoutine = async (routine: RoutineType) => {
-    try {
-        const response = await fetch('http://localhost:3000/api/protected/routines', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                "credentials": "include"
-            },
-            body: JSON.stringify(routine)
-        })
-        const res = await response.json();
-        return res.data;
-    } catch (e) {
-        console.error(e);
-    }
-}
+  try {
+    const response = await fetch(
+      "http://localhost:3000/api/protected/routines",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          credentials: "include",
+        },
+        body: JSON.stringify(routine),
+      },
+    );
+    const res = await resp sgonse.json();
+    return res.data;
+  } catch (e) {
+    console.error(e);
+  }
+};
 
-export const getRoutinesByPlanId = async (plan_id: string) => {
-    try {
-        const response = await fetch(`http://localhost:3000/api/protected/routines/plan/${plan_id}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                "credentials": "include"
-            }
-        })
-        const res = await response.json();
-        return res.data;    
-    } catch (e) {
-        console.error(e);
-    }
-}
+export const getRoutinesByPlanId = async (
+  plan_id: string,
+  searchKey?: string,
+) => {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/api/protected/routines/plan/${plan_id}?search=${searchKey}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          credentials: "include",
+        },
+      },
+    );
+    const res = await response.json();
+    return res.data;
+  } catch (e) {
+    console.error(e);
+  }
+};
 
-export const getRoutinesByUserId = async (user_id: string, currentPlan: string, searchKey?: string ) => {
-    try {
-        const response = await fetch(`http://localhost:3000/api/protected/routines/user/${user_id}?current=${currentPlan}&search=${searchKey}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                "credentials": "include"
-            }
-        })
-        const res = await response.json();
-        return res.data;    
-    } catch (e) {
-        console.error(e);
-    }
-}
+export const getRoutinesByUserId = async (
+  user_id: string,
+  currentPlan: string,
+  searchKey?: string,
+) => {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/api/protected/routines/user/${user_id}?current=${currentPlan}&search=${searchKey}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          credentials: "include",
+        },
+      },
+    );
+    const res = await response.json();
+    return res.data;
+  } catch (e) {
+    console.error(e);
+  }
+};
 
 export const deleteRoutine = async (id: string) => {
-    try {
-        const response = await fetch(`http://localhost:3000/api/protected/routines/${id}`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-                "credentials": "include"
-            }
-        })
-        const res = await response.json();
-        return res.data;
-    } catch (e) {
-        console.error(e)
-    }
-}
+  try {
+    const response = await fetch(
+      `http://localhost:3000/api/protected/routines/${id}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          credentials: "include",
+        },
+      },
+    );
+    const res = await response.json();
+    return res.data;
+  } catch (e) {
+    console.error(e);
+  }
+};
