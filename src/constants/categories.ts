3af8501cd9cd4cfbf0cf066d9b3e79a39e8f5556
Aungheinminn <aungheinnmin@gmail.com>
2024-12-29 22:52:09 +0630

    add transaction categories and note ui

---
+++ src/constants/categories.ts
@@ -0,0 +1,93 @@
+import foodAndBaverageIcon from "../assets/food_and_beverage.svg";
+import transportationIcon from "../assets/transportation.svg";
+import rentalsIcon from "../assets/rental.svg";
+import billsIcon from "../assets/bill.svg";
+import medicalCheckupIcon from "../assets/medical_checkup.svg";
+import educationIcon from "../assets/education.svg";
+import petsIcon from "../assets/pets.svg";
+import fitnessIcon from "../assets/fitness.svg";
+import giftAndDonationsIcon from "../assets/gifts_and_donations.svg";
+import funMoneyIcon from "../assets/fun_money.svg";
+import investmentIcon from "../assets/investment-com.svg";
+import outgoingTransferIcon from "../assets/outgoing_transfer.svg";
+import withdrawFeeIcon from "../assets/withdraw_fee.svg";
+import snackIcon from "../assets/snack.svg";
+import otherIcon from "../assets/other.svg";
+
+export const Categories = [
+  {
+    id: 1,
+    name: "Food & Baverage",
+    icon: foodAndBaverageIcon,
+  },
+  {
+    id: 2,
+    name: "Transportation",
+    icon: transportationIcon,
+  },
+  {
+    id: 3,
+    name: "Rantals",
+    icon: rentalsIcon,
+  },
+  {
+    id: 4,
+    name: "Bills",
+    icon: billsIcon,
+  },
+  {
+    id: 5,
+    name: "Medical Check-up",
+    icon: medicalCheckupIcon,
+  },
+  {
+    id: 6,
+    name: "Education",
+    icon: educationIcon,
+  },
+  {
+    id: 7,
+    name: "Pets",
+    icon: petsIcon,
+  },
+  {
+    id: 8,
+    name: "Fitness",
+    icon: fitnessIcon,
+  },
+  {
+    id: 9,
+    name: "Gift & Donations",
+    icon: giftAndDonationsIcon,
+  },
+  {
+    id: 10,
+    name: "Fun Money",
+    icon: funMoneyIcon,
+  },
+  {
+    id: 11,
+    name: "Investment",
+    icon: investmentIcon,
+  },
+  {
+    id: 12,
+    name: "Outgoing transfer",
+    icon: outgoingTransferIcon,
+  },
+  {
+    id: 13,
+    name: "Withdraw fee",
+    icon: withdrawFeeIcon,
+  },
+  {
+    id: 14,
+    name: "Snack",
+    icon: snackIcon,
+  },
+  {
+    id: 15,
+    name: "Other",
+    icon: otherIcon,
+  },
+];
