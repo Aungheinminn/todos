2c83bfd33419948847fd00955dce5953e46f8c40
Aungheinminn <aungheinnmin@gmail.com>
2025-01-10 22:06:31 +0630

    get transactions by date api

---
+++ src/app/api/protected/transactions/wallet/[wallet_id]/by-transaction-date/route.ts
@@ -0,0 +1,44 @@
+import clientPromise from "@/lib/database";
+import { NextRequest, NextResponse } from "next/server";
+export const GET = async (
+  req: NextRequest,
+  { params }: { params: { wallet_id: string } },
+) => {
+  if (!params.wallet_id) {
+    return NextResponse.json({ status: 400 });
+  }
+  const { wallet_id } = params;
+  const urlParams = new URL(req.url);
+  const transaction_month = urlParams.searchParams.get("transaction_month");
+  const transaction_year = urlParams.searchParams.get("transaction_year");
+
+  try {
+    const client = await clientPromise;
+    const db = client.db("remarker_next");
+    const transactions = db
+      .collection("transactions")
+      .find({
+        wallet_id: wallet_id,
+        transaction_month: transaction_month,
+        transaction_year: transaction_year,
+      })
+      .toArray();
+
+    if (!transactions) {
+      return NextResponse.json(
+        { success: false, error: "Transactions are not found" },
+        { status: 404 },
+      );
+    }
+    return NextResponse.json(
+      { success: true, data: transactions },
+      { status: 200 },
+    );
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json(
+      { success: false, error: "Internal server error" },
+      { status: 500 },
+    );
+  }
+};
