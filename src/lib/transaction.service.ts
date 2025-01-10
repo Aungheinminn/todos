aae4b528e53e25a8a0cbcdea67d2f770a668c788
Aungheinminn <aungheinnmin@gmail.com>
2025-01-10 22:06:31 +0630

    get transactions by date api

--- src/lib/transaction.service.ts
+++ src/lib/transaction.service.ts
@@ -12,6 +12,28 @@ export const getTransactionsByWallet = async (wallet_id: string) => {
   }
 };
 
+export const getTransactionsByDate = async (
+  wallet_id: string,
+  month: number,
+  year: number,
+) => {
+  try {
+    const response = await fetch(
+      `http://localhost:3000/api/protected/transactions/wallet/${wallet_id}/by-transaction-date/?transaction_month=${month}&transaction_year=${year}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+    );
+    const res = await response.json();
+    return res.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 export const postTransactionByWallet = async (data: TransactionType) => {
   try {
     const response = await fetch(
@@ -22,7 +44,7 @@ export const postTransactionByWallet = async (data: TransactionType) => {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(data),
-      }
+      },
     );
     const res = await response.json();
     return res;
