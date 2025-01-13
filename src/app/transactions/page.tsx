fb9a47f88eb3249386deb53701309ea3c5696a6b
Aungheinminn <aungheinnmin@gmail.com>
2025-01-13 22:20:17 +0630

    snap test

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState, useEffect, useRef } from "react";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import wallet from "@/assets/wallet_2.svg";
@@ -7,6 +9,18 @@ import TransactionGroup from "@/components/TransactionsGroup/TransactionGroup";
 import TransactionLoading from "./loading";
 import { useCurrentUserStore } from "@/lib/userStore";
 import { useWalletStore } from "@/lib/walletStore";
+
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+  type CarouselApi,
+} from "@/components/ui/carousel";
+import { mock } from "node:test";
+import CarouselComponent from "@/components/CarouselComponent/CarouselComponent";
 
 const TransactionHeader = () => {
   return (
@@ -22,16 +36,38 @@ const TransactionHeader = () => {
   );
 };
 const Transactions = () => {
-  const { currentUser } = useCurrentUserStore(state => state);
-  const { currentWallet } = useWalletStore(state => state);
+  const { currentUser } = useCurrentUserStore((state) => state);
+  const { currentWallet } = useWalletStore((state) => state);
 
-  const { data: wallet } = useQuery({})
+  const mockData = [
+    { id: 1, day: "1", color: "bg-red-500" },
+    { id: 2, day: "2", color: "bg-sky-500" },
+    { id: 3, day: "3", color: "bg-green-500" },
+    { id: 4, day: "4", color: "bg-purple-500" },
+    { id: 5, day: "5", color: "bg-yellow-500" },
+    { id: 6, day: "6", color: "bg-blue-500" },
+    { id: 7, day: "7", color: "bg-red-500" },
+    { id: 8, day: "8", color: "bg-sky-500" },
+    { id: 9, day: "9", color: "bg-green-500" },
+    { id: 10, day: "10", color: "bg-purple-500" },
+    { id: 11, day: "11", color: "bg-yellow-500" },
+    { id: 12, day: "12", color: "bg-blue-500" },
+
+  ];
+
   return (
     <Suspense fallback={<TransactionLoading />}>
       <div className="w-full px-0 mx-0 bg-gray-800 pt-[55px]">
         <TransactionHeader />
         <div className="w-full flex justify-center items-center">
           <TransactionGroup />
+        </div>
+        <div className="w-full snap-x snap-mandatory flex overflow-x-auto">
+          {mockData.map((item) => (
+            <div key={item.id} className={`w-full snap-center h-[200px] flex-shrink-0 ${item.color}`}>
+              {item.id}
+            </div>
+          ))}
         </div>
       </div>
     </Suspense>
