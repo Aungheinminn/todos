846306f7d01957e59c1e86c8842584b44062379b
Aungheinminn <aungheinnmin@gmail.com>
2025-01-15 22:45:39 +0630

    integrate geTransactionstByDate api with month picker ui

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -9,18 +9,9 @@ import TransactionGroup from "@/components/TransactionsGroup/TransactionGroup";
 import TransactionLoading from "./loading";
 import { useCurrentUserStore } from "@/lib/userStore";
 import { useWalletStore } from "@/lib/walletStore";
-
-import { Card, CardContent } from "@/components/ui/card";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-  type CarouselApi,
-} from "@/components/ui/carousel";
-import { mock } from "node:test";
-import CarouselComponent from "@/components/CarouselComponent/CarouselComponent";
+import { useQuery } from "react-query";
+import { getTransactionsByDate } from "@/lib/transaction.service";
+import TransactionMonthPicker from "@/components/TransactionMonthPicker/TransactionMonthPicker";
 
 const TransactionHeader = () => {
   return (
@@ -38,36 +29,27 @@ const TransactionHeader = () => {
 const Transactions = () => {
   const { currentUser } = useCurrentUserStore((state) => state);
   const { currentWallet } = useWalletStore((state) => state);
+  const [date, setDate] = useState({
+    month: new Date().getMonth() + 1,
+    year: new Date().getFullYear(),
+  });
 
-  const mockData = [
-    { id: 1, day: "1", color: "bg-red-500" },
-    { id: 2, day: "2", color: "bg-sky-500" },
-    { id: 3, day: "3", color: "bg-green-500" },
-    { id: 4, day: "4", color: "bg-purple-500" },
-    { id: 5, day: "5", color: "bg-yellow-500" },
-    { id: 6, day: "6", color: "bg-blue-500" },
-    { id: 7, day: "7", color: "bg-red-500" },
-    { id: 8, day: "8", color: "bg-sky-500" },
-    { id: 9, day: "9", color: "bg-green-500" },
-    { id: 10, day: "10", color: "bg-purple-500" },
-    { id: 11, day: "11", color: "bg-yellow-500" },
-    { id: 12, day: "12", color: "bg-blue-500" },
+  const { data: transactions } = useQuery({
+    queryKey: ["transactions", date],
+    queryFn: () =>
+      getTransactionsByDate(currentWallet?._id || "", date.month, date.year),
+    enabled: !!currentWallet?._id || !!currentUser?._id,
+  });
 
-  ];
+  console.log(transactions);
 
   return (
     <Suspense fallback={<TransactionLoading />}>
-      <div className="w-full px-0 mx-0 bg-gray-800 pt-[55px]">
+      <div className="w-full flex flex-col items-center justify-center  px-0 mx-0 bg-gray-800 pt-[55px]">
         <TransactionHeader />
+        <TransactionMonthPicker date={date} setDate={setDate} />
         <div className="w-full flex justify-center items-center">
           <TransactionGroup />
-        </div>
-        <div className="w-full snap-x snap-mandatory flex overflow-x-auto">
-          {mockData.map((item) => (
-            <div key={item.id} className={`w-full snap-center h-[200px] flex-shrink-0 ${item.color}`}>
-              {item.id}
-            </div>
-          ))}
         </div>
       </div>
     </Suspense>
