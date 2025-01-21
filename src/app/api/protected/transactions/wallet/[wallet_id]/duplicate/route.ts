5f98dc8bf924cdf215f94701aedbc003fd0e3090
Aungheinminn <aungheinnmin@gmail.com>
2025-01-21 22:20:16 +0630

    transaction loading ui and duplicate api

---
+++ src/app/api/protected/transactions/wallet/[wallet_id]/duplicate/route.ts
@@ -0,0 +1,46 @@
+import clientPromise from "@/lib/database";
+import { NextRequest, NextResponse } from "next/server";
+
+export const POST = async (
+  req: NextRequest,
+  {
+    params,
+  }: {
+    params: {
+      wallet_id: string;
+    };
+  },
+) => {
+  if (!params) {
+    return NextResponse.json(
+      { success: false, error: "wallet_id is required" },
+      { status: 400 },
+    );
+  }
+  const { wallet_id } = params;
+  const body = await req.json();
+
+  try {
+    const client = await clientPromise;
+    const db = client.db("remarker_next");
+    const transaction = await db.collection("transactions").find({
+			wallet_id: wallet_id
+		}).insertOne(body);
+    if (!transaction) {
+      return NextResponse.json(
+        { success: false, error: "Error Duplicating" },
+        { status: 404 },
+      );
+    }
+    return NextResponse.json(
+      { success: true, data: transaction },
+      { status: 200 },
+    );
+  } catch (e) {
+    console.error(e);
+    return NextResponse.json(
+      { success: false, error: "Internal server error" },
+      { status: 500 },
+    );
+  }
+};
